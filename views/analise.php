711d43e6c4f3c83fa241d19d69bc236a6ac9150e
joseveronez <jozeveronez@gmail.com>
2017-10-09 13:07:30 -0300

    Analise
    ajustes página

--- views/analise.php
+++ views/analise.php
@@ -69,8 +69,8 @@
                 </div>
             </div>
             
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet fermentum mi eget maximus. Suspendisse potenti. Ut feugiat ligula vitae tortor varius, id blandit est condimentum. Donec bibendum metus nec nisl fringilla, ac ultricies lectus sagittis. Aliquam aliquam imperdiet est in mollis. Nunc sagittis, nisi nec scelerisque gravida, erat enim hendrerit enim, nec facilisis est nisi sit amet est. Integer quis euismod est.
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT3 MarginB1 text-justify">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet fermentum mi eget maximus. Suspendisse potenti. Ut feugiat ligula vitae tortor varius, id blandit est condimentum. Donec bibendum metus nec nisl fringilla, ac ultricies lectus sagittis. Aliquam aliquam imperdiet est in mollis. Nunc sagittis, nisi nec scelerisque gravida, erat enim hendrerit enim, nec facilisis est nisi sit amet est. Integer quis euismod est.
 
 Phasellus porta quis lacus non scelerisque. Nulla venenatis porta luctus. Aliquam bibendum ultrices vehicula. In consequat, dui a cursus elementum, nulla tellus porta turpis, et hendrerit ante metus a mauris. Maecenas leo velit, cursus nec nisi vitae, consequat pretium purus. Pellentesque a risus iaculis, suscipit eros non, dictum nibh. Morbi ut aliquam nibh. Suspendisse vitae ex tortor. Phasellus ut est nunc. Nunc nec diam quis eros pulvinar posuere. Pellentesque posuere nunc in est facilisis scelerisque. Praesent ac eros venenatis, vulputate risus quis, lacinia sapien. Donec ut ornare sem. Integer mollis ullamcorper ante eu lobortis. Nulla facilisi. Pellentesque arcu turpis, semper a justo vel, fringilla iaculis felis.
 
@@ -81,23 +81,115 @@ Vestibulum pulvinar, dolor sed elementum rutrum, tellus ipsum tempor nunc, at gr
 Donec faucibus tellus dui, non placerat nisi dignissim facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas hendrerit laoreet elit id rhoncus. Nam viverra odio eu dapibus semper. Nam libero odio, iaculis eu dolor nec, imperdiet fermentum sapien. Maecenas in nulla augue. Cras sodales quis nunc a vestibulum. Aenean pretium suscipit sapien a blandit. Pellentesque orci dolor, pulvinar vel purus eu, rutrum tempus augue. Nunc sollicitudin, eros dapibus tempor accumsan, neque tortor finibus nisl, sit amet dapibus ligula leo et leo.
             </div>
             
-            <div class="slider-pro col-lg-12 col-md-12 col-sm-12 col-xs-12" id="my-slider">
+            <div class="slider-pro col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero" id="my-slider">
 	           <div class="sp-slides">
 		          <!-- Slide 1 -->
 		          <div class="sp-slide">
-		          	<img class="sp-image" src="imagens/teste.jpg"/>
+                      <img class="sp-image" src="imagens/teste.jpg"/>
+                      <img class="sp-thumbnail" src="imagens/teste.jpg"/>
 		          </div>
 		          
 		          <!-- Slide 2 -->
 		          <div class="sp-slide">
-		          	<img class="sp-image" src="imagens/teste.jpg"/>
+                      <img class="sp-image" src="imagens/teste.jpg"/>
+                      <img class="sp-thumbnail" src="imagens/teste.jpg"/>
 		          </div>
 		          
 		          <!-- Slide 3 -->
 		          <div class="sp-slide">
-		          	<img class="sp-image" src="imagens/teste.jpg"/>
+                      <img class="sp-image" src="imagens/teste.jpg"/>
+                      <img class="sp-thumbnail" src="imagens/teste.jpg"/>
 		          </div>
                 </div>
+            </div>
+            
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT1 text-justify">
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque laoreet fermentum mi eget maximus. Suspendisse potenti. Ut feugiat ligula vitae tortor varius, id blandit est condimentum. Donec bibendum metus nec nisl fringilla, ac ultricies lectus sagittis. Aliquam aliquam imperdiet est in mollis. Nunc sagittis, nisi nec scelerisque gravida, erat enim hendrerit enim, nec facilisis est nisi sit amet est. Integer quis euismod est.
+
+Phasellus porta quis lacus non scelerisque. Nulla venenatis porta luctus. Aliquam bibendum ultrices vehicula. In consequat, dui a cursus elementum, nulla tellus porta turpis, et hendrerit ante metus a mauris. Maecenas leo velit, cursus nec nisi vitae, consequat pretium purus. Pellentesque a risus iaculis, suscipit eros non, dictum nibh. Morbi ut aliquam nibh. Suspendisse vitae ex tortor. Phasellus ut est nunc. Nunc nec diam quis eros pulvinar posuere. Pellentesque posuere nunc in est facilisis scelerisque. Praesent ac eros venenatis, vulputate risus quis, lacinia sapien. Donec ut ornare sem. Integer mollis ullamcorper ante eu lobortis. Nulla facilisi. Pellentesque arcu turpis, semper a justo vel, fringilla iaculis felis.
+
+Phasellus elit ex, iaculis non purus a, venenatis cursus quam. Curabitur rutrum quis nisi id viverra. Nam porta placerat dolor sed efficitur. Ut quis semper turpis, id varius dui. Nulla iaculis maximus volutpat. Curabitur fringilla nisl vel neque condimentum porttitor. Proin sem mi, placerat quis ligula cursus, elementum dignissim massa. Nullam velit nulla, sodales tristique lobortis nec, maximus quis sapien. Nullam non nibh viverra, fermentum nisl ut, tempus quam. Fusce semper, neque id rutrum porta, elit mi rhoncus erat, et tempus urna nisl non diam. Curabitur quis justo eu lacus congue eleifend. Fusce non dolor nec urna molestie pellentesque ac non lacus. Praesent eu risus aliquet, tristique ligula ac, gravida metus. Mauris nec lectus a leo vehicula molestie ac sit amet ante. Sed nec bibendum magna.
+
+Vestibulum pulvinar, dolor sed elementum rutrum, tellus ipsum tempor nunc, at gravida nisl quam in ante. Curabitur diam elit, dictum eget dui ut, molestie condimentum eros. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum volutpat consequat imperdiet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam ac mi ut massa tincidunt dignissim. Curabitur suscipit tempus elit nec pulvinar.
+
+Donec faucibus tellus dui, non placerat nisi dignissim facilisis. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas hendrerit laoreet elit id rhoncus. Nam viverra odio eu dapibus semper. Nam libero odio, iaculis eu dolor nec, imperdiet fermentum sapien. Maecenas in nulla augue. Cras sodales quis nunc a vestibulum. Aenean pretium suscipit sapien a blandit. Pellentesque orci dolor, pulvinar vel purus eu, rutrum tempus augue. Nunc sollicitudin, eros dapibus tempor accumsan, neque tortor finibus nisl, sit amet dapibus ligula leo et leo.
+            </div>
+            
+            <table class="table col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT1">
+                <thead>
+                    <tr>
+                        <th>Lorem</th>
+                        <th>Lorem</th>
+                        <th>Lorem</th>
+                        <th>Lorem</th>
+                        <th>Lorem</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                    </tr>
+                    <tr>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                    </tr>
+                    <tr>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                    </tr>
+                </tbody>
+            </table>
+            
+            <table class="table col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT1">
+                <tbody>
+                    <tr>
+                        <th>Lorem</th>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                    </tr>
+                    <tr>
+                        <th>Lorem</th>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                    </tr>
+                    <tr>
+                        <th>Lorem</th>
+                        <td>Ipsum</td>
+                        <td>Ipsum</td>
+                    </tr>
+                </tbody>
+            </table>
+            
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT1 MarginB1 text-justify">
+                <a href="#"><div class="col-lg-2 tag text-center preto-fonte">Lorem</div></a>
+                <a href="#"><div class="col-lg-2 tag text-center preto-fonte">Lorem</div></a>
+                <a href="#"><div class="col-lg-2 tag text-center preto-fonte">Lorem</div></a>
+                <a href="#"><div class="col-lg-2 tag text-center preto-fonte">Lorem</div></a>
+                <a href="#"><div class="col-lg-2 tag text-center preto-fonte">Lorem</div></a>
+                <a href="#"><div class="col-lg-2 tag text-center preto-fonte">Lorem</div></a>
+            </div>
+            
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT1 MarginB1 bg-cinza-claro">
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3">
+                    <h4>COMPARTILHE:</h4>
+                </div>
+                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+                
+                </div>
+            </div>
+            
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 padding-zero MarginT1 MarginB1 bg-cinza-claro">
+                
             </div>
             
         </div>
@@ -109,6 +201,10 @@ Donec faucibus tellus dui, non placerat nisi dignissim facilisis. Pellentesque h
               width: '100%',
               height: 350,
               buttons: true,
+              arrows: true,
+              loop: true,
+              thumbnailArrows: true,
+              thumbnailPosition: 'bottom',
           });
         });
 </script>
