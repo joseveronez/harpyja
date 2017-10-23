33153a4f8bf6f3033071175ede1ca71516b280c1
Nícolas Sampaio <sampaio.anything@gmail.com>
2017-10-23 13:49:50 -0200

    Site
    Ajustes finos na home
    > Ajustes de espaçamento
    > Fonte
    > Alinhamento

--- views/home.php
+++ views/home.php
@@ -1,102 +1,122 @@
-<div class="slider-pro MarginT10" id="my-slider">
+<style type="text/css">
+    @media screen and (min-width: 768px) {
+        #my-slider { margin-top: 137px }
+    }
+    @media screen and (max-width: 767px) {
+        #my-slider { margin-top: 70px }
+    }
+
+    .bloco-galeria .conteudo { margin-top: 50px; margin-bottom: 65px }
+    .bloco-galeria .conteudo .titulo { margin-bottom: 25px }
+    .bloco-titulo .conteudo { margin-top: 50px; margin-bottom: 25px }
+    .bloco-sobre .conteudo { padding-top: 65px; padding-bottom: 50px }
+    .bloco-sobre .conteudo p { font-size: 16px; text-align: justify }
+</style>
+<div class="slider-pro" id="my-slider">
     <div class="sp-slides">
         <div class="sp-slide">
-            <img class="sp-image" src="imagens/teste.jpg"/>
+            <img class="sp-image" src="<?= RAIZSITE ?>/imagens/teste.jpg">
         </div>
         <div class="sp-slide">
-            <img class="sp-image" src="imagens/teste.jpg"/>
+            <img class="sp-image" src="<?= RAIZSITE ?>/imagens/teste.jpg">
         </div>
         <div class="sp-slide">
-            <img class="sp-image" src="imagens/teste.jpg"/>
+            <img class="sp-image" src="<?= RAIZSITE ?>/imagens/teste.jpg">
         </div>
     </div>
 </div>
 
-
-<div class="container MarginT5 MarginB5">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h1>
-            Lorem ipsum
-            <hr class="hrTitle hrPadrao hidden-sm hidden-xs">
-        </h1>
-        <h2>
-            Lorem ipsum dolor sit amet.
-        </h2>
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bloco-titulo padding-zero">
+    <div class="container conteudo">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+            <h1>
+                Lorem ipsum
+                <hr class="hrTitle hrPadrao hidden-sm hidden-xs">
+            </h1>
+            <h2>
+                Lorem ipsum dolor sit amet.
+            </h2>
+        </div>
     </div>
 </div>
 
-<div class="container MarginT5 MarginB5">
-    <div class="" style="margin-top: 5px;">
-        <div class="slider-pro sp-vertical" id="my-slider2">
-            <div class="sp-slides">
-                <div class="sp-slide">
-                    <img class="sp-image" src="imagens/teste.jpg"/>
-                    <a href="" class="title-noticia" target="_self">
-                        <h5 class="text-noticia">
-                            <b>Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing</b>
-                        </h5>
-                    </a>
-                    <div class="sp-thumbnail">
-                        <div class="sp-thumbnail-image-container" style="background-image: url(imagens/teste.jpg); background-position: center center; background-repeat: no-repeat; background-size: cover;">
-                        </div>
-                        <div class="sp-thumbnail-text">
-                            <div class="sp-thumbnail-title">Lorem ipsum</div>
-                            <div class="sp-thumbnail-description">Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="sp-slide">
-                    <img class="sp-image" src="imagens/teste.jpg"/>
-                    <a href="" class="title-noticia" target="_self">
-                        <h5 class="text-noticia">
-                            <b>Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing</b>
-                        </h5>
-                    </a>
-                    <div class="sp-thumbnail">
-                        <div class="sp-thumbnail-image-container" style="background-image: url(imagens/teste.jpg); background-position: center center; background-repeat: no-repeat; background-size: cover;">
-                        </div>
-                        <div class="sp-thumbnail-text">
-                            <div class="sp-thumbnail-title">Lorem ipsum</div>
-                            <div class="sp-thumbnail-description">Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="sp-slide">
-                    <img class="sp-image" src="imagens/teste.jpg"/>
-                    <a href="" class="title-noticia" target="_self">
-                        <h5 class="text-noticia">
-                            <b>Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing</b>
-                        </h5>
-                    </a>
-                    <div class="sp-thumbnail">
-                        <div class="sp-thumbnail-image-container" style="background-image: url(imagens/teste.jpg); background-position: center center; background-repeat: no-repeat; background-size: cover;">
-                        </div>
-                        <div class="sp-thumbnail-text">
-                            <div class="sp-thumbnail-title">Lorem ipsum</div>
-                            <div class="sp-thumbnail-description">Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>    
-    </div>
-</div>
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-cinza-claro MarginT5 MarginB5">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
     <div class="container">
-        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
-            <img class="img-responsive" src="<?= RAIZSITE ?>/imagens/autor.png"/ style="margin: 0 auto;">
-        </div>
-        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
-            <h3>Lorem ipsum</h3>
-            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu felis quis lorem volutpat vestibulum. In quis vulputate dolor, eget feugiat neque. Donec mauris tellus, vulputate ut ligula eget, ullamcorper semper erat. Donec nulla tortor, malesuada ac nisi vitae, porta interdum magna. Suspendisse ex elit, rhoncus nec risus et, interdum gravida lacus.</h5>
+        <div class="" style="margin-top: 5px;">
+            <div class="slider-pro sp-vertical" id="my-slider2">
+                <div class="sp-slides">
+                    <div class="sp-slide">
+                        <img class="sp-image" src="imagens/teste.jpg"/>
+                        <a href="" class="title-noticia" target="_self">
+                            <h5 class="text-noticia">
+                                <b>Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing</b>
+                            </h5>
+                        </a>
+                        <div class="sp-thumbnail">
+                            <div class="sp-thumbnail-image-container" style="background-image: url(imagens/teste.jpg); background-position: center center; background-repeat: no-repeat; background-size: cover;">
+                            </div>
+                            <div class="sp-thumbnail-text">
+                                <div class="sp-thumbnail-title">Lorem ipsum</div>
+                                <div class="sp-thumbnail-description">Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sp-slide">
+                        <img class="sp-image" src="imagens/teste.jpg"/>
+                        <a href="" class="title-noticia" target="_self">
+                            <h5 class="text-noticia">
+                                <b>Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing</b>
+                            </h5>
+                        </a>
+                        <div class="sp-thumbnail">
+                            <div class="sp-thumbnail-image-container" style="background-image: url(imagens/teste.jpg); background-position: center center; background-repeat: no-repeat; background-size: cover;">
+                            </div>
+                            <div class="sp-thumbnail-text">
+                                <div class="sp-thumbnail-title">Lorem ipsum</div>
+                                <div class="sp-thumbnail-description">Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sp-slide">
+                        <img class="sp-image" src="imagens/teste.jpg"/>
+                        <a href="" class="title-noticia" target="_self">
+                            <h5 class="text-noticia">
+                                <b>Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing</b>
+                            </h5>
+                        </a>
+                        <div class="sp-thumbnail">
+                            <div class="sp-thumbnail-image-container" style="background-image: url(imagens/teste.jpg); background-position: center center; background-repeat: no-repeat; background-size: cover;">
+                            </div>
+                            <div class="sp-thumbnail-text">
+                                <div class="sp-thumbnail-title">Lorem ipsum</div>
+                                <div class="sp-thumbnail-description">Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing Dolor sit amet, consectetur adipiscing </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>    
         </div>
     </div>
 </div>
 
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginT5 MarginB5">
-    <div class="container">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-cinza-claro bloco-sobre">
+    <div class="container conteudo">
+        <div class="col-lg-2 col-md-2 col-sm-1 hidden-xs">&nbsp;</div>
+        <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+            <img class="img-responsive img-circle" src="<?= RAIZSITE ?>/imagens/autor.png" style="margin: 0 auto;"><br>
+        </div>
+        <div class="col-lg-5 col-md-5 col-sm-6 col-xs-12">
+            <h3 class="margin-zero">Lorem ipsum</h3><br>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eu felis quis lorem volutpat vestibulum. In quis vulputate dolor, eget feugiat neque. Donec mauris tellus, vulputate ut ligula eget, ullamcorper semper erat. Donec nulla tortor, malesuada ac nisi vitae, porta interdum magna. Suspendisse ex elit, rhoncus nec risus et, interdum gravida lacus.</p>
+        </div>
+        <div class="col-lg-2 col-md-2 col-sm-1 hidden-xs">&nbsp;</div>
+    </div>
+</div>
+
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bloco-galeria">
+    <div class="container conteudo">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-            <h2>GALERIA DE VIDEOS</h2>
+            <p class="size30 margin-zero titulo">GALERIA DE VÍDEOS</p>
         </div>
         <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 video-container">
             <iframe src="https://www.youtube.com/embed/7RxeORpEmEs" frameborder="0" allowfullscreen></iframe>
@@ -130,7 +150,7 @@
     </div>
 </div>
 
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-cinza-claro parallax-img parallax-default MarginT5 MarginB5">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-cinza-claro parallax-img parallax-default">
     <div class="container"></div>
 </div>
 
