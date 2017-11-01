ed91655ee5284b1cee165e91dc7a87036bbd999c
SampaioO <sampaio.anything@gmail.com>
2017-11-01 08:07:49 -0200

    Site
    Ajustes de alinhamento foram realizados, foram inclusos hovers em alguns botões, foram padronizados mais alguns espaçamentos e foi incluso a tag em php RAIZSITE, pois em algumas páginas estava faltando

--- views/componentes/footer.php
+++ views/componentes/footer.php
@@ -1,12 +1,13 @@
 <style type="text/css">
     input[type="text"].form-control, input[type="email"].form-control, textarea.form-control { border-radius: 0; border: 1px solid #444142; }
+    input[type="email"].form-control.email { width: 100% !important; }
     button[type="submit"] { border-radius: 0; background: transparent; color: #444142; border: 1px solid #444142; }
     button[type="submit"]:hover { background: #444142; color: #ffffff; border: 1px solid #444142; }
 
     .bg-cinza-alternativo { background: #d9d9d9 }
 
     @media screen and (min-width: 768px) {
-        footer .bloco-logo { height: 380px }
+        footer .bloco-logo { margin-top: 42px }
         footer .bloco-links { margin-top: 50px }
         footer .bloco-formulario { margin-top: 107px }
         footer .bloco-mapa { margin-top: 50px; margin-bottom: 45px }
@@ -22,6 +23,8 @@
         footer .bloco-formulario { margin-bottom: 40px }
         footer .bloco-mapa { margin-bottom: 40px }
         footer .bloco-mapa iframe { width: 100%; }
+
+        .mobile-zero { padding: 0; }
     }
 </style>
 <div id="fb-root"></div>
@@ -36,25 +39,25 @@
     <?php if($pagina_atual != "contato") { ?>
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 bg-cinza-footer">
         <div class="container Relative">
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 bloco-logo">
-                <div class="conteudo">
-                    <a href="<?= RAIZSITE ?>"><img src="<?= RAIZSITE ?>/imagens/logo-sem-fundo.png" class="img-responsive"></a><br>
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
-                        <h4 style="margin-top: 0px;">ACOMPANHE A HARPYJA</h4>
-                        <hr class="hrTitleFull hrPadrao">
-                    </div>
-                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero ">
-                        <h6 class="MarginT2p MarginB4p">Fique por dentro de novas avaliações e dicas</h6>
-                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero">
-                            <h6 class="MarginT2p MarginB01p">Email</h6>
-                            <input type="email" class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12 MarginB1p padding-zero">
-                            <button type="button" class="btn btn-default btn-assinar">Assinar</button>
-                        </div>
-                    </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 bloco-logo mobile-zero">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero ">
+	                <a href="<?= RAIZSITE ?>"><img src="<?= RAIZSITE ?>/imagens/logo-sem-fundo.png" class="img-responsive"></a><br>
+	            </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero MarginT3p">
+                    <h4 class="size16" style="margin-top: 0px;">ACOMPANHE A HARPYJA</h4>
+                    <hr class="hrTitleFull hrPadrao">
+                </div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-zero ">
+                    <h6 class="MarginT2p MarginB4p">Fique por dentro de novas avaliações e dicas</h6>
                     
+                    <form action="" class="form-horizontal MarginT9p">
+                        <input type="email" class="form-control email" name="email" placeholder="Digite seu e-mail*"><br>
+
+                        <button type="submit" class="btn btn-default btn-assinar">Assinar</button>
+                    </form>
                 </div>
             </div>
-           <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 bloco-formulario">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 bloco-formulario mobile-zero">
                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 padding-menu-footer">
                    <div class="text-center size18">
                        <h5><a href="<?= RAIZSITE ?>" class="link-default preto-fonte">Home</a></h5>
@@ -67,7 +70,7 @@
                    </div>
                 </div>
             </div>       
-            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 bloco-mapa">
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 bloco-mapa mobile-zero">
                 <div class="fb-page" data-href="https://www.facebook.com/facebook" data-tabs="timeline" data-height="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/facebook" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/facebook"></a></blockquote></div>
             </div>
         </div>
